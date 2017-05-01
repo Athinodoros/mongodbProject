bd06083ea66a19d6d9a8998cd979240a84f8b8d9
biggiejr <jrmacej@gmail.com>
2017-05-01 19:30:18 +0200

    Modified No. 1

--- facade/querstion1.js
+++ facade/querstion1.js
@@ -6,13 +6,9 @@
 
 
 function getAllTweets(db){
-    return db.collection('tweets').find({}).toArray();
+    return db.collection('tweets').distinct("user").length.toArray();
 }
-function getAllTweetsCount(db){
-    return db.collection('tweets').aggregate([{ $group : { _id : "$id", }, $group : {_id : 0,count: { $sum: 1 }}}]).toArray();
 
-}
 module.exports = {
-    getAllTweetsCount:getAllTweetsCount,
     getAllTweets:getAllTweets
 };
